034917fe3db4eb1d17dd97b02fddb111574de9ef
manu-tg <emmanueltorrestdad@gmail.com>
2022-06-27 19:51:09 -0400

    Fix loginrequerid in ViewTests

--- tests/presenters/abstract.py
+++ tests/presenters/abstract.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.urls import resolve
 from django.test import TestCase, Client
 
@@ -11,39 +12,58 @@ class ViewTest():
     def setUp(self):
         # need a browser customer 
         self.client = Client()
-        
+    def get_url(self):
+        """Return url for test_root_url and test_return_correct_out
+        Those are base case by test
+        """
+        return self.url
+      
     def get_data(self):
         """Generate a correct data
         """
         pass
+    
     def test_root_url(self):
-        """The view is the correct"""
-        found = resolve(self.url)
+        """Check the view is the correct"""
+        found = resolve(self.get_url())
+        print("function name: {} {}----------".format(found.func.__name__, self.function.__name__))
         self.assertEqual(found.func.__name__, self.function.__name__)
         
     def test_return_correct_out(self):
+        """Test response from client.get() or client.post
+        - Test status code
+        - Test out file is correct or redirect link if have redirect
+        - Test context data
+        """
         response = self.get_response()
         self.assertEqual(response.status_code,self.status_code)
         self.check_out(response)
+        self.check_context_data(response)
       
     def uses_redirect(self, response):
-        self.assertEqualI(response['location'], self.redirect)
+        self.assertEqual(response['location'], self.redirect)
     
     def check_out(self, response):
         if self.redirect is None:
             return self.check_html(response)  
         else:
             self.uses_redirect(response)   
-            
+    
+    def check_context_data(self, response):
+        pass
+          
     def check_html(self, response):
         # TODO: Cuando integren los html
         # self.is_working(response)
-        self.uses_template(response)            
+        self.uses_template(response)
                       
     def uses_template(self, response):
         """Checking what templated are used"""
-        print("respomse --------------",response)
+        prueba = response
         self.assertTemplateUsed(response,self.template)
+        print("pase---------------------------")            
+        print("pase---------------------------")            
+        
     
     def is_working(self, response):
         """Just to make sure everything is working"""
@@ -59,16 +79,16 @@ class ViewTest():
 class PostViewTest(ViewTest):
     
     def get_response(self):
-        response = self.client.post(self.url, data=self.get_data())
+        response = self.client.post(self.get_url(), data=self.get_data())
         return response
                    
 class GetViewTest(ViewTest):
 
     def get_response(self):
-        response = self.client.get(self.url, data=self.get_data())
+        response = self.client.get(self.get_url(), data=self.get_data())
         return response
      
-
+# class PostWhitUserTest()
 # TODO: Test de get contex data
 # class HomePageTest(TestCase):
 #     def test_environment_set_in_context(self):
--- tests/presenters/player/test.py
+++ tests/presenters/player/test.py
@@ -1,48 +1,63 @@
 from django.test import TestCase, RequestFactory
 from skyrim.data.models import *
-from skyrim.presenters.player.views import PlayerByUserView, PlayerView
-from tests.presenters.abstract import ViewTest, GetViewTest
+from skyrim.presenters.player.views import  PlayerView
+from tests.presenters.abstract import ViewTest, GetViewTest, PostViewTest
 from skyrim.presenters.views import create_player_view
-               
-class ListPlayerTest():
-    def setUp(self):
-        # need a browser customer 
-        self.factory = RequestFactory()
-        self.user = User.objects.create(username ='fred',email ='secret',
-                                        password='top_secret')        
 
-class CreatePlayerTest(ViewTest,TestCase):
+# class ListPlayerTest():
+#     def setUp(self):
+#         # need a browser customer
+#         self.factory = RequestFactory()
+#         self.user = User.objects.create(username ='fred',email ='secret',
+#                                         password='top_secret')
+
+class CreatePlayerTest(PostViewTest,TestCase):
     """CreatePlayerTest"""
     function = create_player_view
     url = '/create_player/'
     template = 'create_player.html'
+    status_code = 302
+    redirect = "/accounts/login/?next="+url
 
     def setUp(self):
         # TODO: Populate para poder testear  get_all_player_races() y get_all_spells()
        super().setUp()
        user,created = User.objects.get_or_create(email= '', username='user1', password='ppfn32123')
        self.client.login(username='user1', password='ppfn32123')
-    
-    def get_response(self):
-        response = self.client.get(self.url)
-        return response
-    
+       
+    def get_data(self):
+        data= {"name":"Robert",
+               "race_id": 1,
+               "health_points":12,
+               "pedro": [1,2]
+        }
+
 
 class GetPlayerTest(GetViewTest,TestCase):
     """CreatePlayerTest"""
-    function = PlayerView().as_view()
-    url = '/create_player/'
-    template = 'create_player.html'
-    status_code = 200
+    function = PlayerView.as_view()
+    url = '/player/'
+    template = 'test copy.html'
+    
 
     def setUp(self):
        super().setUp()
        fire = DamageType.objects.create(type = 'fire')
+       fire.save()
        paladin = Race.objects.create(race_name = 'paladin',weakness = fire)
-       user, created = User.objects.get_or_create(email= '', username='user1', password='ppfn32123')
+       paladin.save()
+       player_race = PlayerRace.objects.create(id_race=paladin)
+       player_race.save()
+       user, created = User.objects.get_or_create(email= 'example@gmail.com', username='user1', password='ppfn32123')
+       user.save()
        character1 = Character.objects.create(character_name = 'character1',race_type = paladin,health_points = 500,id_client= user)
-       player1 = Player.objects.create(id_character = character1)
-       
-    def get_response(self):
-        response = self.client.get(self.url+"1/")
-        return response    
+       character1.save()
+       player1 = Player(id_character = character1)
+       player1.save()
+       arkigj = player1
+    
+    def get_url(self):
+        return self.url+"1/"
+
+    def check_context_data(self, response):
+        return super().check_context_data(response)
